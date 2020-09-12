48b836d9d1424487348768145904a2399e8a5d56
Pratinavagarwala <66483787+Pratinavagarwala@users.noreply.github.com>
2020-09-12 21:56:11 +0530

    Update Oil.js

--- js/Oil.js
+++ js/Oil.js
@@ -1,9 +1,19 @@
 class Oil {
 
     constructor() {
-      
+      this.c=createElement('h4');
+      this.k=createElement('h4');
+      this.p=createElement('h4');
+      this.d=createElement('h4');
     }
     display(){
-        
+        this.c.html("The Current Value of CRUDE OIL per litre :5,000 ");
+        this.c.position(200,200);
+        this.k.html("The Current Value of KEROSENE per litre :11.20 ");
+        this.k.position(300,200);
+        this.p.html("The Current Value of PATROL per litre :77.50 ");
+        this.p.position(400,200);
+        this.d.html("The Current Value of DIESEL per gram :65.50 ");
+        this.d.position(500,200);
     }
 }
